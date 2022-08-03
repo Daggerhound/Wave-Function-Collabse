27d6e53a7df4f7bd49bea629f6c249035aa5bba8
Daggerhound <daggyhound@gmail.com>
2022-08-03 15:03:01 +0000

    red square

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from panda3d.core import CardMaker, NodePath
 from direct.showbase.ShowBase import ShowBase
 
 class App(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
+
+        self.cm = CardMaker("squares") # cm for card maker
+        self.cm.setFrame(-0.5,0.5,-0.5,0.5)
+
+        myFrames = self.aspect2d.attachNewNode('frames')
+
+        myFrame = myFrames.attachNewNode(self.cm.generate())
+        myFrame.setColor(255,0,0,1)
+
 
 app = App()
 app.run()
