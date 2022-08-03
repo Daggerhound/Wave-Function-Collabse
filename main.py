b08a1f6e0b8e2e8cb9a0a3a66ea938dfb6a8ab89
Daggerhound <daggyhound@gmail.com>
2022-08-03 15:30:35 +0000

    squares

--- main.py
+++ main.py
@@ -6,13 +6,28 @@ class App(ShowBase):
         ShowBase.__init__(self)
 
         self.cm = CardMaker("squares") # cm for card maker
-        self.cm.setFrame(-0.5,0.5,-0.5,0.5) # sets size
 
         squares = self.aspect2d.attachNewNode('squares') 
 
+        # Red Square on upper left quarter
+        self.cm.setFrame(-1, 0, 1, 0)
         square = squares.attachNewNode(self.cm.generate())
         square.setColor(255,0,0,1)
 
+        # Blue Square on upper right quarter
+        self.cm.setFrame(0, 1, 1, 0)
+        square = squares.attachNewNode(self.cm.generate())
+        square.setColor(0,0,255,1)
+
+        # Blue Square on lower left quarter
+        self.cm.setFrame(-1, 0, 0, -1)
+        square = squares.attachNewNode(self.cm.generate())
+        square.setColor(0,0,255,1)
+
+        # Red Square on lower right quarter
+        self.cm.setFrame(0, 1, 0, -1)
+        square = squares.attachNewNode(self.cm.generate())
+        square.setColor(255,0,0,1)
 
 app = App()
 app.run()
