48931b8f51f17346613947793ca20625cfc1d392
Daggerhound <daggyhound@gmail.com>
2022-08-04 23:44:01 +0000

    organization and infrasctuture

--- main.py
+++ main.py
@@ -1,50 +1,17 @@
-from panda3d.core import CardMaker
-from direct.showbase.ShowBase import ShowBase
+from grid import Grid
 
-class App(ShowBase):
+
+# constants
+RES   = (11, 11) # rows, cols
+
+
+class App:
     def __init__(self):
-        ShowBase.__init__(self)
+        self.grid = Grid(RES)
+        self.grid.generate()        
 
-        self.cm = CardMaker("squares") # cm for card maker
-
-        self.squares = self.aspect2d.attachNewNode('squares') 
-
-        res = [11,11] # [x,y]
-        x = 2 # abs lenght from left to right
-        y = 2 # abs heigh from up to down
-
-        cx = x/res[0]
-        cy = y/res[1]
-        toggle = False
-        for resY in range(res[1]):
-            for resX in range(res[0]):
-                left = -1 + cx*resX
-                right = -1 + cx*(resX+1)
-                bot = -1 + cy*(resY+1)
-                top = -1 + cy*resY
-                
-                if toggle:
-                    self.blue_square(left, right, bot, top)
-                else:
-                    self.red_square(left, right, bot, top)
-                
-                toggle = not toggle
-
-            if res[0] % 2 == 0:
-                toggle = not resY%2
-
-        
-
-    def blue_square(self,left, right, bot, top):
-        self.cm.setFrame(left, right, bot, top)
-        self.square = self.squares.attachNewNode(self.cm.generate())
-        self.square.setColor(0,0,255,1)
-
-    def red_square(self,left, right, bot, top):
-        self.cm.setFrame(left, right, bot, top)
-        self.square = self.squares.attachNewNode(self.cm.generate())
-        self.square.setColor(255,0,0,1)
-
+    def run(self): # Need to find a better way
+        self.grid.run()
 
 app = App()
 app.run()
