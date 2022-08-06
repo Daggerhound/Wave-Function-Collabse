5fbb3767c75cef77d1ed5e263cfec064fa61ccf3
Daggerhound <daggyhound@gmail.com>
2022-08-06 08:35:09 +0000

    added /src

---
+++ src/cell.py
@@ -0,0 +1,12 @@
+class Cell:
+    def __init__(self, x, y, square):
+        self.x = x
+        self.y = y
+        self.square = square # will be changed to self.tile later
+        self.choices = 0 # entropy, should be length of tileset by default 
+
+    def show(self):
+        self.square.show()
+
+    def hide(self):
+        self.square.hide()
---
+++ src/main.py
@@ -0,0 +1,17 @@
+from grid import Grid
+
+
+# constants
+RES   = (11, 11) # rows, cols
+
+
+class App:
+    def __init__(self):
+        self.grid = Grid(RES)
+        self.grid.generate()        
+
+    def run(self): # Need to find a better way
+        self.grid.run()
+
+app = App()
+app.run()
